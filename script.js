65c3045a9d16057622313f98a20b0097c01b67a9
nushratlifestyle <nushratlifestyle@gmail.com>
2026-07-14 01:30:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,90 @@ let nav = document.querySelector("nav");
 nav.classList.toggle("active");
 
 }
+let cart = 0;
+
+
+function addCart(){
+
+cart++;
+
+document.getElementById("cartCount").innerHTML = cart;
+
+}
+
+
+
+
+
+function filterProduct(category){
+
+
+let products = document.querySelectorAll(".product-card");
+
+
+products.forEach(function(product){
+
+
+if(category=="all"){
+
+product.style.display="block";
+
+}
+
+else if(product.classList.contains(category)){
+
+product.style.display="block";
+
+}
+
+else{
+
+product.style.display="none";
+
+}
+
+
+});
+
+
+}
+
+
+
+
+
+// SEARCH PRODUCT
+
+
+document.getElementById("searchBox").addEventListener("keyup",function(){
+
+
+let value=this.value.toLowerCase();
+
+
+let products=document.querySelectorAll(".product-card");
+
+
+products.forEach(function(product){
+
+
+let text=product.innerText.toLowerCase();
+
+
+if(text.includes(value)){
+
+product.style.display="block";
+
+}
+
+else{
+
+product.style.display="none";
+
+}
+
+
+});
+
+
+});
